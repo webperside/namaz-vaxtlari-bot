ff5de71095a77a05f8230aed8e269f178030c520
hamidsultanzadeh <sultanzade.06@mail.ru>
2020-11-15 10:26:19 +0400

    init

---
+++ src/main/java/com/webperside/namazvaxtlariibot/NamazVaxtlariBotApplication.java
@@ -0,0 +1,13 @@
+package com.webperside.namazvaxtlariibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamazVaxtlariBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NamazVaxtlariBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/webperside/namazvaxtlariibot/NamazVaxtlariBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webperside.namazvaxtlariibot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamazVaxtlariBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
