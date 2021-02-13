61c5f8dd15d3926453b2d28004a583ba9a797d6f
hamidsultanzadeh <sultanzade.06@mail.ru>
2021-02-13 11:45:47 +0400

    update listener fixed and logs created

--- src/main/java/com/webperside/namazvaxtlaribot/telegram/TelegramListener.java
+++ src/main/java/com/webperside/namazvaxtlaribot/telegram/TelegramListener.java
@@ -4,27 +4,47 @@ import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
+import static com.webperside.namazvaxtlaribot.config.Constants.ADMIN_TELEGRAM_ID;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class TelegramListener {
 
     private final TelegramService telegramService;
 
-    public void listener(){
+    public void listener() {
         TelegramConfig.getInstance().setUpdatesListener(updates -> {
 
+            log.info("Updates size : [{}]", updates.size());
+
             for (Update update : updates) {
-                TelegramConfig.execute(new SendMessage("625929111",String.valueOf(update.message().from().id())));
-//                TelegramConfig.execute(new SendMessage(update.message().from().id(),"Istifadeniz uchun teshekkurler !!!"));
+                long userId = update.message().from().id();
+
+                log.info("Working on Update ID : [{}]", update.updateId());
+                TelegramConfig.execute(new SendMessage(ADMIN_TELEGRAM_ID, userId + " new user ID"));
+
                 try {
                     telegramService.process(update);
-                } catch (IOException e) {
-                    System.out.println(update);
-                    e.printStackTrace();
+                } catch (Exception e) {
+
+                    final String ERROR_MESSAGE_TEMPLATE = "USER ID - %s\n" +
+                            "EXCEPTION - %s\n" +
+                            "MESSAGE - %s";
+
+                    log.error("User ID [{}], Exception [{}], Message [{}]", userId, e.getClass().getSimpleName(), e.getMessage());
+
+                    TelegramConfig.execute(new SendMessage(ADMIN_TELEGRAM_ID,
+                                    String.format(
+                                            ERROR_MESSAGE_TEMPLATE,
+                                            userId,
+                                            e.getClass().getSimpleName(),
+                                            e.getMessage())
+                            )
+                    );
                     return update.updateId();
                 }
             }
