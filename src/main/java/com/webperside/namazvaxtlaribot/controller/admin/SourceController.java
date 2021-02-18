2bd9b4236daf44620a70c2b0aabdda0e511d6f98
hamidsultanzadeh <sultanzade.06@mail.ru>
2021-02-18 12:59:59 +0400

    sources page

---
+++ src/main/java/com/webperside/namazvaxtlaribot/controller/admin/SourceController.java
@@ -0,0 +1,36 @@
+package com.webperside.namazvaxtlaribot.controller.admin;
+
+import com.webperside.namazvaxtlaribot.dto.rest.SourceDto;
+import com.webperside.namazvaxtlaribot.models.Source;
+import com.webperside.namazvaxtlaribot.service.SourceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/admin/sources")
+@RequiredArgsConstructor
+public class SourceController {
+
+    private final SourceService sourceService;
+
+    @GetMapping
+    public ModelAndView sources(ModelAndView modelAndView){
+        Page<Source> sources = sourceService.getAll(0);
+        modelAndView.addObject("sources", sources);
+        modelAndView.addObject("newSource", new SourceDto());
+        modelAndView.setViewName("admin-panel/sources");
+        return modelAndView;
+    }
+
+    @PostMapping("/edit")
+    public String editSource(@ModelAttribute(value = "newSource") SourceDto sourceDto){
+        sourceService.save(SourceDto.toEntity(sourceDto));
+        return "redirect:/admin/sources";
+    }
+}
---
+++ src/main/java/com/webperside/namazvaxtlaribot/controller/rest/SourceRestController.java
@@ -0,0 +1,23 @@
+package com.webperside.namazvaxtlaribot.controller.rest;
+
+import com.webperside.namazvaxtlaribot.dto.rest.SourceDto;
+import com.webperside.namazvaxtlaribot.service.SourceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/api/sources")
+@RequiredArgsConstructor
+public class SourceRestController {
+
+    private final SourceService sourceService;
+
+    @GetMapping("{sourceId}")
+    public SourceDto getById(@PathVariable("sourceId") Integer sourceId){
+        return SourceDto.fromEntity(sourceService.findById(sourceId));
+    }
+
+}
