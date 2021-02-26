9d9d20a545d3c3632976bd2a96a548fba2c3cbd0
hamidsultanzadeh <sultanzade.06@mail.ru>
2021-02-26 16:40:06 +0400

    development by userStatus

--- src/main/java/com/webperside/namazvaxtlaribot/controller/admin/UserController.java
+++ src/main/java/com/webperside/namazvaxtlaribot/controller/admin/UserController.java
@@ -1,18 +1,17 @@
 package com.webperside.namazvaxtlaribot.controller.admin;
 
-import com.webperside.namazvaxtlaribot.config.Constants;
 import com.webperside.namazvaxtlaribot.dto.view.SendMessageDto;
 import com.webperside.namazvaxtlaribot.dto.view.UserDto;
 import com.webperside.namazvaxtlaribot.service.UserService;
+import com.webperside.namazvaxtlaribot.util.SortParams;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin/users")
@@ -22,11 +21,16 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public ModelAndView users(ModelAndView modelAndView){
-        List<UserDto> userDtoList = userService.getAllWithInfo(0);
-        modelAndView.addObject("users", userDtoList);
+    public ModelAndView users(@RequestParam(value = "p", required = false, defaultValue = "0") Integer page,
+                              @RequestParam(value = "sort", required = false, defaultValue = "createdAt:desc") String sortBy,
+                              ModelAndView modelAndView){
+        String[] sortParams = sortBy.split(":");
+        Page<UserDto> userDtoList = userService.getAllWithInfo(page, sortParams);
 
+        modelAndView.addObject("users", userDtoList);
+        modelAndView.addObject("sortedBy", SortParams.fieldName(sortParams));
         modelAndView.addObject("sendMessage", new SendMessageDto());
+        modelAndView.addObject("sortParam", sortBy);
 
         modelAndView.setViewName("admin-panel/users");
         return modelAndView;
